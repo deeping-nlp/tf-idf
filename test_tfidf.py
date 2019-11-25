ac0b768135e12c3fcc99ef33e42c347e2a54e2e2
qiangyongjun <qiangyj@feidai.com>
2019-11-25 14:06:47 +0800

    tf-idf

---
+++ test_tfidf.py
@@ -0,0 +1,23 @@
+from TfIdf import TfIdf
+
+
+class TestTfIdf:
+    def test_similarity(self):
+        table = TfIdf()
+        table.add_document("doc1", ["The", "game", "of", "life", "is", "a", "game", "of", "everlasting", "learning"])
+        table.add_document("doc2", ["The", "unexamined", "life", "is", "not", "worth", "living"])
+        table.add_document("doc3", ["Never", "stop", "learning"])
+
+        table.calculate_tf()
+        table.calculate_idf()
+        table.calculate_tf_idf()
+
+
+
+        sims = table.similarities(["life","learning"])
+        return sims
+
+
+if __name__ == "__main__":
+    testTfIdf_similarity = TestTfIdf().test_similarity()
+    print(testTfIdf_similarity)
